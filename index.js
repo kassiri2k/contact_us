17f187fb3f4c02b07e5b8fd9eb34e64c9d12bf29
Assiri Karim <mirak94@hotmail.com>
2023-04-29 13:27:46 +0200

    add comments

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+// To store variables environment
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+
 app = express();
+
+// for parsing the data of the request, use json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// set the engine for rendering
 app.set('view engine', 'ejs');
 
+//Get request
 app.get('/contact', (req, res) => {
     res.render('index')
 })
 
-
+//Post request
 app.post('/contact', (req, res) => {
     const {
         name,
@@ -24,6 +30,8 @@ app.post('/contact', (req, res) => {
 
     res.send('Thanks for your message!');
 });
+
 port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Server is running at ${port}
 `))
